8c345e6ab01a9936e3987ca511cbf0efa547f9c3
rodrigopk <rodrigopk@gmail.com>
2018-03-04 11:56:44 +0100

    Add environment configuration to project

--- components/Home/actions.js
+++ components/Home/actions.js
@@ -1,4 +1,6 @@
-const API_BASE_URL = 'https://totally-not-twiter.herokuapp.com/';
+import Config from 'react-native-config'
+
+const API_BASE_URL = Config.API_BASE_URL;
 const HELLO_ENDPOINT = 'api/hello';
 
 export const MESSAGE_RECEIVED = 'MESSAGE_RECEIVED';
